9a0075fe9f8788e28be4f8e4ba55738942166702
richardl2003 <richardliy03@gmail.com>
2022-08-15 21:36:35 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Banner } from './components/Banner'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
 import { About } from './components/About'
+import { Footer } from './components/Footer'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap'
+
+
+export const Footer = () => {
+    return (
+        <footer className='footer'>
+            <Container>
+                <p className='text-center'>&copy; 2022 Richard Li</p>
+            </Container>
+        </footer>
+    )
+}
