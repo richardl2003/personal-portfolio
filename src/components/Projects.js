589a6e63f7311b26b6e7a0d9fb717684c79cb5f1
richardl2003 <richardliy03@gmail.com>
2022-08-16 21:29:10 -0400

    Edited projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,48 +1,37 @@
-import { Nav, Col, Row, Tab, Container } from "react-bootstrap";
+import { Col, Row, Tab, Container } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png"
-import projImg2 from "../assets/img/project-img2.png"
-import projImg3 from "../assets/img/project-img3.png"
+import gitAPI from "../assets/img/gitAPI.png"
+import spending from "../assets/img/gitAPI.png"
+import tictactoe from "../assets/img/tictactoe.png"
+import minesweeper from "../assets/img/minesweeper.png"
 
 export const Projects = () => {
 
     const projects = [
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-            linkUrl: "https://github.com/richardl2003/"
+            title: "Git Commands REST API",
+            description: "Full Stack Web Application to remember commonly used Git Commands",
+            imgUrl: gitAPI,
+            linkUrl: "https://github.com/richardl2003/git_commands_RestAPI"
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-            linkUrl: "https://github.com/richardl2003/"
+            title: "Transaction Data Analyzer",
+            description: "Tracks spending habits visually",
+            imgUrl: spending,
+            linkUrl: "https://github.com/richardl2003/transaction_tracker"
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-            linkUrl: "https://github.com/richardl2003/"
+            title: "Tictactoe AI",
+            description: "Utilized the Minimax algorithm to never lose",
+            imgUrl: tictactoe,
+            linkUrl: "https://github.com/richardl2003/tictactoe"
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-            linkUrl: "https://github.com/richardl2003/"
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-            linkUrl: "https://github.com/richardl2003/"
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-            linkUrl: "https://github.com/richardl2003/"
-        },
+            title: "Minesweeper AI",
+            description: "Prepositional Logic AI to play optimally",
+            imgUrl: minesweeper,
+            linkUrl: "https://github.com/richardl2003/Minesweeper"
+        }
     ]
 
 
@@ -52,19 +41,8 @@ export const Projects = () => {
                 <Row>
                     <Col>
                         <h2>Projects</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                        <p>Here are some of the personal projects I have been working on: </p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
                             <Tab.Content>
                                 <Tab.Pane eventKey="first">
                                     <Row>
@@ -80,8 +58,6 @@ export const Projects = () => {
                                         }
                                     </Row>
                                 </Tab.Pane>
-                                <Tab.Pane eventKey="second">tab 2</Tab.Pane>
-                                <Tab.Pane eventKey="third">tab 3</Tab.Pane>
                             </Tab.Content>
                         </Tab.Container>
                     </Col>
