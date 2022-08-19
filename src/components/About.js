ceb3fb3e0b8aed269e95eaf239af8954e8264ead
richardl2003 <richardliy03@gmail.com>
2022-08-19 12:47:34 -0400

    Added link to poster in about

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ export const About = () => {
                     </p>
                     <p className='lead text-left'>
                         I love the thrill of learning new technical skills as there are numerous applications that technology can help advance our society. Most recently, I finished my first Co-op working as a 
-                        research assistant at McMaster University investigating merge conflicts in Git.
+                        research assistant at McMaster University investigating merge conflicts in Git. You can view a poster on my work <a href='https://drive.google.com/file/d/1S4VcQcO3BuCY1cKwBrjBvTJ8yrBD6efq/view?usp=sharing'>here</a>.
                     </p>
                     <p className='lead text-left'>
                         I hope to continue to gain experience in the field of Software Engineering, as I have a keen interest in DevOps, Full Stack Development, and Artifical Intelligence.
